04fd61ecbad8569bfb7bdbd0c04fb4bd5cd8f169
Joel Damata <joel.damata94@gmail.com>
2020-08-09 16:24:59 -0400

    add webhook flag

--- cmd/drone-teams/config.go
+++ cmd/drone-teams/config.go
@@ -6,13 +6,18 @@
 package main
 
 import (
-	"github.com/urfave/cli/v2"
 	"github.com/jdamata/drone-teams/plugin"
+	"github.com/urfave/cli/v2"
 )
 
 // settingsFlags has the cli.Flags for the plugin.Settings.
 func settingsFlags(settings *plugin.Settings) []cli.Flag {
-	// Replace below with all the flags required for the plugin.
-	// Use Destination within the cli.Flags to populate settings
-	return []cli.Flag{}
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:        "webhook",
+			Usage:       "MS teams connector webhook endpoint",
+			EnvVars:     []string{"PLUGIN_WEBHOOK"},
+			Destination: &settings.Webhook,
+		},
+	}
 }
